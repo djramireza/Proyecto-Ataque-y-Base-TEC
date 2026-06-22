5557c8fee41564177fe815449e4dce42f1d969ac
G.Valerio <85315212+gabopos1@users.noreply.github.com>
2026-06-21 20:17:30 -0600

    Modify cooldown for 'Torre mágica'
    Updated the cooldown of 'Torre mágica' from 2 to 6.

--- core/data_definitions.py
+++ core/data_definitions.py
@@ -1,7 +1,7 @@
 TOWERS_CATALOG = {
     "basica": {"nombre": "Torre básica", "coste": 50, "hp": 100, "daño": 10, "rango": 3, "habilidad": "disparo_doble", "cooldown": 3},
     "pesada": {"nombre": "Torre pesada", "coste": 130, "hp": 250, "daño": 20, "rango": 2, "habilidad": "boost_temporal", "cooldown": 2},
-    "magica": {"nombre": "Torre mágica", "coste": 120, "hp": 80, "daño": 9, "rango": 4, "habilidad": "curar_torre", "cooldown": 2},
+    "magica": {"nombre": "Torre mágica", "coste": 120, "hp": 80, "daño": 9, "rango": 4, "habilidad": "curar_torre", "cooldown": 6},
 }
 
 UNITS_CATALOG = {
